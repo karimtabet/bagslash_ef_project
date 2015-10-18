26e13dfd0ccd38dee88b1228d5850ee1a2279cb5
Karim Tabet <karim@karimtabet.com>
2015-10-18 17:26:45 +0100

    remove redundant comma

--- web/config.py
+++ web/config.py
@@ -10,5 +10,5 @@ class BaseConfig(object):
     DB_PORT = os.environ['DB_PORT']
     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
         DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
-    ),
+    )
     SECRET_KEY = os.environ['SECRET_KEY']
