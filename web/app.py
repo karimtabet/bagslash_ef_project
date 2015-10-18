51f6a1e63272f3dfcfdc108b64512f4862e3a6bc
Karim Tabet <karim@karimtabet.com>
2015-10-18 17:27:10 +0100

    flask admin initialisation

--- web/app.py
+++ web/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
+from models import Redirect
 from config import BaseConfig
 
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
+admin = Admin(app, template_mode='bootstrap3')
 
+admin.add_view(ModelView(Redirect, db.session))
 
 @app.route('/')
 def index():
