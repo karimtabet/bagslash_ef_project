826f06b62e65351c63c9d5c5388cc1c4616a0935
Karim Tabet <karim@karimtabet.com>
2015-10-19 11:54:58 +0100

    insert 1 redirect util function and test value of return_redirect function

--- web/tests.py
+++ web/tests.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask.ext.testing import TestCase
 from hamcrest import assert_that, is_
 
-from app import app, db
+from app import app, db, get_redirect
 from models import Base, Redirect
 
 
@@ -17,15 +17,25 @@ class TestRedirects(TestCase):
         for table in reversed(Base.metadata.sorted_tables):
             db.session.execute(table.delete())
 
-    def test_successful_redirect(self):
+    def insert_redirect(self):
         redirect = Redirect(
             from_url='test_url',
-            to_url='www.example.com',
+            to_url='https://www.example.com',
             times_accessed=0,
             date_created=datetime.utcnow()
         )
         db.session.add(redirect)
+        return redirect
 
+    def test_get_redirect(self):
+        self.insert_redirect()
+        assert_that(
+            get_redirect('test_url'),
+            is_('https://www.example.com')
+        )
+
+    def test_successful_redirect(self):
+        self.insert_redirect()
         response = self.app.get('/test_url')
         assert_that(
             response.status_code,
