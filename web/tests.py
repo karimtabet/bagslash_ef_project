9f74dfedd0175bed3eb772c4c132f69942c589e5
Karim Tabet <karim@karimtabet.com>
2015-10-19 15:23:20 +0100

    adjust tests to insert user

--- web/tests.py
+++ web/tests.py
@@ -4,7 +4,7 @@ from flask.ext.testing import TestCase
 from hamcrest import assert_that, is_, has_length
 
 from app import app, db, get_redirect
-from models import Base, Redirect
+from models import Base, Redirect, User
 from utils import get_random_string, add_http_to_url
 
 
@@ -18,11 +18,19 @@ class TestRedirects(TestCase):
         for table in reversed(Base.metadata.sorted_tables):
             db.session.execute(table.delete())
 
+    def insert_user(self):
+        user = User(name='Test User')
+        db.session.add(user)
+        db.session.commit()
+        return user
+
     def insert_redirect(self):
+        user = self.insert_user()
         redirect = Redirect(
             from_url='test_url',
             to_url='https://www.example.com',
             times_accessed=0,
+            created_by=user.uuid.hex,
             date_created=datetime.utcnow()
         )
         db.session.add(redirect)
